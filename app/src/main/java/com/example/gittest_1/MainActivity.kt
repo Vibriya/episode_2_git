1d88757514e8dc43a2d1d26b961b35ea5fef66f6
Margarita <ritaboyko906151@gmail.com>
2023-10-08 18:00:50 +0300

    Add views to MainActivity.kt

--- app/src/main/java/com/example/gittest_1/MainActivity.kt
+++ app/src/main/java/com/example/gittest_1/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.gittest_1
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view: View? = null
+        val view2: View? = null
     }
 }
